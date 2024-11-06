1db913247dd239d7e55668c7e153c752ff81b18a
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-11-06 20:26:58 -0300

    .

--- Tests/LogicaTests.cs
+++ Tests/LogicaTests.cs
@@ -7,16 +7,10 @@ public class LogicaTests
     [Test]
     public void PruebaPosicionPokemon()
     {
-        CreadorDePokemonYMovimiento creadorDePokemonYMovimiento = new CreadorDePokemonYMovimiento();
-        Logica logica = new Logica();
-        Assert.That(logica.listaPokemon[0].Nombre == "Venusaur");
     }
     
     [Test]
     public void PruebaEstadisticaPokemon()
     {
-        CreadorDePokemonYMovimiento creadorDePokemonYMovimiento = new CreadorDePokemonYMovimiento();
-        Logica logica = new Logica();
-        Assert.That(logica.listaPokemon[1].VidaMax == 100);
     }
 }
