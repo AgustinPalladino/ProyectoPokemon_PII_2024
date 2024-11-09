74efba45c3d907b53d68e9c1453ea06199fc8ca0
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-11-09 19:33:15 -0300

    .

--- Tests/OperacionesStaticTests.cs
+++ Tests/OperacionesStaticTests.cs
@@ -35,14 +35,14 @@ namespace LibraryTests
         [Test]
         public void CalcularCritico_GolpeCritico_DeberiaRetornar1_2()
         {
-            double resultado = OperacionesStatic.CalcularCritico("Fuego", "Agua");
+            double resultado = OperacionesStatic.CalcularCritico(100);
             Assert.AreEqual(1.2, resultado, 0.1); 
         }
 
         [Test]
         public void CalcularCritico_SinGolpeCritico_DeberiaRetornar1_0()
         {
-            double resultado = OperacionesStatic.CalcularCritico("Agua", "Fuego");
+            double resultado = OperacionesStatic.CalcularCritico(60);
             Assert.AreEqual(1.0, resultado);
         }
     }
