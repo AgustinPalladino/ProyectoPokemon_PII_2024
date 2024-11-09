f6cd5206cc81336e9cb4f450325c8e5506ffe922
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-11-09 20:56:36 -0300

    Agrego pokemon y movimientos

--- Library/CreadorDePokemonYMovimiento.cs
+++ Library/CreadorDePokemonYMovimiento.cs
@@ -36,6 +36,15 @@ namespace Library
             Pokemon groudon = new Pokemon("Groudon", "Tierra", 150, 100, 80);
             Pokemon muk = new Pokemon("Muk", "Veneno", 120, 60, 60);
             Pokemon snorlax = new Pokemon("Snorlax", "Normal", 160, 85, 70);
+            Pokemon clefable = new Pokemon("Clefable", "Hada", 95, 70, 65);
+            Pokemon steelix = new Pokemon("Steelix", "Acero", 120, 150, 70);
+            Pokemon kyogre = new Pokemon("Kyogre", "Agua", 150, 100, 80);
+            Pokemon roserade = new Pokemon("Roserade", "Planta", 80, 65, 75);
+            Pokemon jolteon = new Pokemon("Jolteon", "Eléctrico", 80, 65, 50);
+            Pokemon zapdos = new Pokemon("Zapdos", "Eléctrico", 120, 90, 60);
+            Pokemon salamence = new Pokemon("Salamence", "Dragón", 140, 110, 80);
+            Pokemon moltres = new Pokemon("Moltres", "Fuego", 130, 100, 70);
+            
             //Ataques
             Movimiento Arañazo = new Movimiento("Arañazo", 5, "Fuego", false);
             Movimiento Lanzallamas = new Movimiento("Lanzallamas", 20, "Fuego", false);
@@ -53,6 +62,18 @@ namespace Library
             Movimiento Envenenar = new Movimiento("Envenenar", 0, "Veneno", true);
             Movimiento Quemar = new Movimiento("Quemar", 0, "Fuego", true);
             Movimiento Paralizar = new Movimiento("Paralizar", 0, "Electrico", true);
+            Movimiento PuñoHielo = new Movimiento("PuñoHielo", 20, "Hielo", false);
+            Movimiento Aguacero = new Movimiento("Aguacero", 20, "Agua", false);
+            Movimiento PuñoElectrico = new Movimiento("PuñoEléctrico", 20, "Eléctrico", false);
+            Movimiento PuñoAereo = new Movimiento("PuñoAéreo", 15, "Volador", false);
+            Movimiento HojaAfilada = new Movimiento("Hoja Afilada", 25, "Planta", false);
+            Movimiento ColaLarga = new Movimiento("Cola Larga", 25, "Dragón", false);
+            Movimiento PuñoLucha = new Movimiento("Puño Lucha", 25, "Lucha", false);
+            Movimiento PuñoFuego = new Movimiento("Puño Fuego", 20, "Fuego", false);
+            Movimiento Hipnosis = new Movimiento("Hipnosis", 0, "Psiquico", true);
+            Movimiento Encanto = new Movimiento("Encanto", 0, "Hada", true);
+            Movimiento Rayo = new Movimiento("Rayo", 20, "Electrico", false);
+            
             // Agregar movimientos a los pokemons
             venusaur.agregarMovimiento(Arañazo);
             venusaur.agregarMovimiento(Lluevehojas);
@@ -81,19 +102,19 @@ namespace Library
             dragonite.agregarMovimiento(Puñofuego);
             dragonite.agregarMovimiento(GarraDragon);
             dragonite.agregarMovimiento(Paralizar);
-            dragonite.agregarMovimiento(Paralizar);
+            dragonite.agregarMovimiento(PuñoAereo);
             articuno.agregarMovimiento(Arañazo);
             articuno.agregarMovimiento(Aéreo);
             articuno.agregarMovimiento(Dormir);
             articuno.agregarMovimiento(Envenenar);
-            machamp.agregarMovimiento(Arañazo);
+            machamp.agregarMovimiento(PuñoLucha);
             machamp.agregarMovimiento(Puñodinámico);
             machamp.agregarMovimiento(Paralizar);
             machamp.agregarMovimiento(Quemar);
             alakazam.agregarMovimiento(Arañazo);
             alakazam.agregarMovimiento(Confusion);
             alakazam.agregarMovimiento(Quemar);
-            alakazam.agregarMovimiento(Envenenar);
+            alakazam.agregarMovimiento(Hipnosis);
             gengar.agregarMovimiento(Arañazo);
             gengar.agregarMovimiento(Confusion);
             gengar.agregarMovimiento(Quemar);
@@ -113,8 +134,43 @@ namespace Library
             snorlax.agregarMovimiento(Arañazo);
             snorlax.agregarMovimiento(Puñodinámico);
             snorlax.agregarMovimiento(Envenenar);
-            snorlax.agregarMovimiento(Dormir);
-
+            snorlax.agregarMovimiento(Rayo);
+            jolteon.agregarMovimiento(Arañazo);
+            jolteon.agregarMovimiento(Electrocañon);
+            jolteon.agregarMovimiento(PuñoElectrico);
+            jolteon.agregarMovimiento(Paralizar);
+            zapdos.agregarMovimiento(Rayo);
+            zapdos.agregarMovimiento(Aéreo);
+            zapdos.agregarMovimiento(PuñoElectrico);
+            zapdos.agregarMovimiento(Dormir);
+            salamence.agregarMovimiento(PuñoFuego);
+            salamence.agregarMovimiento(GarraDragon);
+            salamence.agregarMovimiento(ColaLarga);
+            salamence.agregarMovimiento(Dormir);
+            moltres.agregarMovimiento(Arañazo);
+            moltres.agregarMovimiento(Aéreo);
+            moltres.agregarMovimiento(PuñoHielo);
+            moltres.agregarMovimiento(Paralizar);
+            gengar.agregarMovimiento(Arañazo);
+            gengar.agregarMovimiento(Confusion);
+            gengar.agregarMovimiento(Quemar);
+            gengar.agregarMovimiento(Envenenar);
+            clefable.agregarMovimiento(Arañazo);
+            clefable.agregarMovimiento(Encanto);
+            clefable.agregarMovimiento(Dormir);
+            clefable.agregarMovimiento(Quemar);
+            steelix.agregarMovimiento(LanzaRocas);
+            steelix.agregarMovimiento(Arañazo);
+            steelix.agregarMovimiento(Dormir);
+            steelix.agregarMovimiento(Envenenar);
+            kyogre.agregarMovimiento(Hidrocañon);
+            kyogre.agregarMovimiento(Arañazo);
+            kyogre.agregarMovimiento(Hidrocañon);
+            kyogre.agregarMovimiento(Aguacero);
+            roserade.agregarMovimiento(Colmilloveneno);
+            roserade.agregarMovimiento(Confusion);
+            roserade.agregarMovimiento(Quemar);
+            roserade.agregarMovimiento(HojaAfilada);
 
 
             // Agregar los pokemons a la lista
@@ -133,6 +189,15 @@ namespace Library
             listaPokemon.Add(groudon);
             listaPokemon.Add(muk);
             listaPokemon.Add(snorlax);
+            listaPokemon.Add(salamence);
+            listaPokemon.Add(moltres);
+            listaPokemon.Add(gengar);
+            listaPokemon.Add(clefable);
+            listaPokemon.Add(steelix);
+            listaPokemon.Add(kyogre);
+            listaPokemon.Add(roserade);
+            listaPokemon.Add(jolteon);
+            listaPokemon.Add(zapdos);
         }
     }
 }
