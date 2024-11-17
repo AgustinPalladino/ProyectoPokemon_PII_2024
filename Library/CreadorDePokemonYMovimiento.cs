6f4413cc8f7a9ee1a157862e9248c8e97dec5c59
Rodrigofagundez2004 <yeekko19@gmail.com>
2024-11-17 19:16:41 -0300

    Cambioe en creador de pokemon y movimientos

--- Library/CreadorDePokemonYMovimiento.cs
+++ Library/CreadorDePokemonYMovimiento.cs
@@ -6,21 +6,23 @@ namespace Library
 {
     public class CreadorDePokemonYMovimiento
     {
-        public List<Pokemon> listaPokemon = new();
-        
+        public Dictionary<string, Pokemon> diccionarioPokemon = new();
+        public Dictionary<string, Movimiento> diccionarioMovimientos = new();
+
         /// <summary>
         /// Constructor.
         /// </summary>
         public CreadorDePokemonYMovimiento()
         {
-            AgregarPokemon();
+            AgregarPokemonYMovimientos();
         }
+
         /// <summary>
         /// Método para agregar Pokemones con sus respectivos movimientos.
         /// </summary>
-        public void AgregarPokemon()
+        public void AgregarPokemonYMovimientos()
         {
-            
+            // Crear los Pokemones
             Pokemon venusaur = new Pokemon("Venusaur", "Planta", 120, 40, 60);
             Pokemon charizard = new Pokemon("Charizard", "Fuego", 100, 60, 40);
             Pokemon blastoise = new Pokemon("Blastoise", "Agua", 110, 50, 50);
@@ -44,158 +46,85 @@ namespace Library
             Pokemon zapdos = new Pokemon("Zapdos", "Eléctrico", 120, 90, 60);
             Pokemon salamence = new Pokemon("Salamence", "Dragón", 140, 110, 80);
             Pokemon moltres = new Pokemon("Moltres", "Fuego", 130, 100, 70);
-            
-            //Ataques
-            Movimiento Arañazo = new Movimiento("Arañazo", 5, 80,"Fuego", false);
-            Movimiento Lanzallamas = new Movimiento("Lanzallamas", 20, 80,"Fuego", false);
-            Movimiento Lluevehojas = new Movimiento("Lluevehojas", 20, 100,"Planta", false);
-            Movimiento Hidrocañon = new Movimiento("Hidrocañon", 20, 60,"Agua", false);
-            Movimiento Puñofuego = new Movimiento("Puñofuego", 20, 60,"Fuego", false);
-            Movimiento Electrocañon = new Movimiento("Electrocañon", 100, 40,"Eléctrico", false);
-            Movimiento Aéreo = new Movimiento("Aéreo", 15, 60,"Volador", false);
-            Movimiento Colmilloveneno = new Movimiento("Colmillo venenoso", 15,60, "Veneno", false);
-            Movimiento Puñodinámico = new Movimiento("Puño dinámico", 25, 100,"Lucha", false);
-            Movimiento GarraDragon = new Movimiento("GarraDragon", 30, 60,"Lucha", false);
-            Movimiento Confusion = new Movimiento("Confusion",50,60,"Psiquico",false);
-            Movimiento LanzaRocas = new Movimiento("LanzaRocas", 20, 80,"Tierra", false);
-            Movimiento Dormir = new Movimiento("Dormir", 0, 100,"Psiquico", true);
-            Movimiento Envenenar = new Movimiento("Envenenar", 0, 100,"Veneno", true);
-            Movimiento Quemar = new Movimiento("Quemar", 0, 100,"Fuego", true);
-            Movimiento Paralizar = new Movimiento("Paralizar", 0,100, "Electrico", true);
-            Movimiento PuñoHielo = new Movimiento("PuñoHielo", 20, 80,"Hielo", false);
-            Movimiento Aguacero = new Movimiento("Aguacero", 20, 60,"Agua", false);
-            Movimiento PuñoElectrico = new Movimiento("PuñoEléctrico", 20, 60,"Eléctrico", false);
-            Movimiento PuñoAereo = new Movimiento("PuñoAéreo", 15, 80,"Volador", false);
-            Movimiento HojaAfilada = new Movimiento("Hoja Afilada", 25, 60,"Planta", false);
-            Movimiento ColaLarga = new Movimiento("Cola Larga", 25, 60,"Dragón", false);
-            Movimiento PuñoLucha = new Movimiento("Puño Lucha", 25, 60,"Lucha", false);
-            Movimiento PuñoFuego = new Movimiento("Puño Fuego", 20, 80,"Fuego", false);
-            Movimiento Rayo = new Movimiento("Rayo", 20, 100,"Electrico", false);
-            
-            // Agregar movimientos a los pokemons
-            venusaur.agregarMovimiento(Arañazo);
-            venusaur.agregarMovimiento(Lluevehojas);
-            venusaur.agregarMovimiento(Dormir);
-            venusaur.agregarMovimiento(Quemar);
-            charizard.agregarMovimiento(Arañazo);
-            charizard.agregarMovimiento(Lanzallamas);
-            charizard.agregarMovimiento(Paralizar);
-            charizard.agregarMovimiento(Quemar);
-            blastoise.agregarMovimiento(Arañazo);
-            blastoise.agregarMovimiento(Hidrocañon);
-            blastoise.agregarMovimiento(Dormir);
-            blastoise.agregarMovimiento(Envenenar);
-            pikachu.agregarMovimiento(Arañazo);
-            pikachu.agregarMovimiento(Electrocañon);
-            pikachu.agregarMovimiento(Envenenar);
-            pikachu.agregarMovimiento(Paralizar);
-            pidgeot.agregarMovimiento(Aéreo);
-            pidgeot.agregarMovimiento(Arañazo);
-            pidgeot.agregarMovimiento(Dormir);
-            pidgeot.agregarMovimiento(Paralizar);
-            butterfree.agregarMovimiento(Colmilloveneno);
-            butterfree.agregarMovimiento(Aéreo);
-            butterfree.agregarMovimiento(Quemar);
-            butterfree.agregarMovimiento(Dormir);
-            dragonite.agregarMovimiento(Puñofuego);
-            dragonite.agregarMovimiento(GarraDragon);
-            dragonite.agregarMovimiento(Paralizar);
-            dragonite.agregarMovimiento(PuñoAereo);
-            articuno.agregarMovimiento(Arañazo);
-            articuno.agregarMovimiento(Aéreo);
-            articuno.agregarMovimiento(Dormir);
-            articuno.agregarMovimiento(Envenenar);
-            machamp.agregarMovimiento(PuñoLucha);
-            machamp.agregarMovimiento(Puñodinámico);
-            machamp.agregarMovimiento(Paralizar);
-            machamp.agregarMovimiento(Quemar);
-            alakazam.agregarMovimiento(Arañazo);
-            alakazam.agregarMovimiento(Confusion);
-            alakazam.agregarMovimiento(Quemar);
-            alakazam.agregarMovimiento(Dormir);
-            gengar.agregarMovimiento(Arañazo);
-            gengar.agregarMovimiento(Confusion);
-            gengar.agregarMovimiento(Quemar);
-            gengar.agregarMovimiento(Envenenar);
-            onix.agregarMovimiento(LanzaRocas);
-            onix.agregarMovimiento(Arañazo);
-            onix.agregarMovimiento(Dormir);
-            onix.agregarMovimiento(Envenenar);
-            groudon.agregarMovimiento(LanzaRocas);
-            groudon.agregarMovimiento(Arañazo);
-            groudon.agregarMovimiento(Envenenar);
-            groudon.agregarMovimiento(Paralizar);
-            muk.agregarMovimiento(Colmilloveneno);
-            muk.agregarMovimiento(Confusion);
-            muk.agregarMovimiento(Dormir);
-            muk.agregarMovimiento(Quemar);
-            snorlax.agregarMovimiento(Arañazo);
-            snorlax.agregarMovimiento(Puñodinámico);
-            snorlax.agregarMovimiento(Envenenar);
-            snorlax.agregarMovimiento(Rayo);
-            jolteon.agregarMovimiento(Arañazo);
-            jolteon.agregarMovimiento(Electrocañon);
-            jolteon.agregarMovimiento(PuñoElectrico);
-            jolteon.agregarMovimiento(Paralizar);
-            zapdos.agregarMovimiento(Rayo);
-            zapdos.agregarMovimiento(Aéreo);
-            zapdos.agregarMovimiento(PuñoElectrico);
-            zapdos.agregarMovimiento(Dormir);
-            salamence.agregarMovimiento(PuñoFuego);
-            salamence.agregarMovimiento(GarraDragon);
-            salamence.agregarMovimiento(ColaLarga);
-            salamence.agregarMovimiento(Dormir);
-            moltres.agregarMovimiento(Arañazo);
-            moltres.agregarMovimiento(Aéreo);
-            moltres.agregarMovimiento(PuñoHielo);
-            moltres.agregarMovimiento(Paralizar);
-            gengar.agregarMovimiento(Arañazo);
-            gengar.agregarMovimiento(Confusion);
-            gengar.agregarMovimiento(Quemar);
-            gengar.agregarMovimiento(Envenenar);
-            clefable.agregarMovimiento(Arañazo);
-            clefable.agregarMovimiento(Envenenar);
-            clefable.agregarMovimiento(Dormir);
-            clefable.agregarMovimiento(Quemar);
-            steelix.agregarMovimiento(LanzaRocas);
-            steelix.agregarMovimiento(Arañazo);
-            steelix.agregarMovimiento(Dormir);
-            steelix.agregarMovimiento(Envenenar);
-            kyogre.agregarMovimiento(Hidrocañon);
-            kyogre.agregarMovimiento(Arañazo);
-            kyogre.agregarMovimiento(Hidrocañon);
-            kyogre.agregarMovimiento(Aguacero);
-            roserade.agregarMovimiento(Colmilloveneno);
-            roserade.agregarMovimiento(Confusion);
-            roserade.agregarMovimiento(Quemar);
-            roserade.agregarMovimiento(HojaAfilada);
 
+            // Crear los movimientos
+            Movimiento Arañazo = new Movimiento("Arañazo", 5, 80, "Fuego", false);
+            Movimiento Lanzallamas = new Movimiento("Lanzallamas", 20, 80, "Fuego", false);
+            Movimiento Lluevehojas = new Movimiento("Lluevehojas", 20, 100, "Planta", false);
+            Movimiento Hidrocañon = new Movimiento("Hidrocañon", 20, 60, "Agua", false);
+            Movimiento Puñofuego = new Movimiento("Puñofuego", 20, 60, "Fuego", false);
+            Movimiento Electrocañon = new Movimiento("Electrocañon", 100, 40, "Eléctrico", false);
+            Movimiento Aéreo = new Movimiento("Aéreo", 15, 60, "Volador", false);
+            Movimiento Colmilloveneno = new Movimiento("Colmillo venenoso", 15, 60, "Veneno", false);
+            Movimiento Puñodinámico = new Movimiento("Puño dinámico", 25, 100, "Lucha", false);
+            Movimiento GarraDragon = new Movimiento("GarraDragon", 30, 60, "Lucha", false);
+            Movimiento Confusion = new Movimiento("Confusion", 50, 60, "Psiquico", false);
+            Movimiento LanzaRocas = new Movimiento("LanzaRocas", 20, 80, "Tierra", false);
+            Movimiento Dormir = new Movimiento("Dormir", 0, 100, "Psiquico", true);
+            Movimiento Envenenar = new Movimiento("Envenenar", 0, 100, "Veneno", true);
+            Movimiento Quemar = new Movimiento("Quemar", 0, 100, "Fuego", true);
+            Movimiento Paralizar = new Movimiento("Paralizar", 0, 100, "Electrico", true);
+            Movimiento PuñoHielo = new Movimiento("PuñoHielo", 20, 80, "Hielo", false);
+            Movimiento Aguacero = new Movimiento("Aguacero", 20, 60, "Agua", false);
+            Movimiento PuñoElectrico = new Movimiento("PuñoEléctrico", 20, 60, "Eléctrico", false);
+            Movimiento PuñoAereo = new Movimiento("PuñoAéreo", 15, 80, "Volador", false);
+            Movimiento HojaAfilada = new Movimiento("Hoja Afilada", 25, 60, "Planta", false);
+            Movimiento ColaLarga = new Movimiento("Cola Larga", 25, 60, "Dragón", false);
+            Movimiento PuñoLucha = new Movimiento("Puño Lucha", 25, 60, "Lucha", false);
+            Movimiento PuñoFuego = new Movimiento("Puño Fuego", 20, 80, "Fuego", false);
+            Movimiento Rayo = new Movimiento("Rayo", 20, 100, "Electrico", false);
 
-            // Agregar los pokemons a la lista
-            listaPokemon.Add(venusaur);
-            listaPokemon.Add(charizard);
-            listaPokemon.Add(blastoise);
-            listaPokemon.Add(pikachu);
-            listaPokemon.Add(pidgeot);
-            listaPokemon.Add(butterfree);
-            listaPokemon.Add(dragonite);
-            listaPokemon.Add(articuno);
-            listaPokemon.Add(machamp);
-            listaPokemon.Add(alakazam);
-            listaPokemon.Add(gengar);
-            listaPokemon.Add(onix);
-            listaPokemon.Add(groudon);
-            listaPokemon.Add(muk);
-            listaPokemon.Add(snorlax);
-            listaPokemon.Add(salamence);
-            listaPokemon.Add(moltres);
-            listaPokemon.Add(gengar);
-            listaPokemon.Add(clefable);
-            listaPokemon.Add(steelix);
-            listaPokemon.Add(kyogre);
-            listaPokemon.Add(roserade);
-            listaPokemon.Add(jolteon);
-            listaPokemon.Add(zapdos);
+            // Agregar los movimientos a un diccionario
+            diccionarioMovimientos.Add("Arañazo", Arañazo);
+            diccionarioMovimientos.Add("Lanzallamas", Lanzallamas);
+            diccionarioMovimientos.Add("Lluevehojas", Lluevehojas);
+            diccionarioMovimientos.Add("Hidrocañon", Hidrocañon);
+            diccionarioMovimientos.Add("Puñofuego", Puñofuego);
+            diccionarioMovimientos.Add("Electrocañon", Electrocañon);
+            diccionarioMovimientos.Add("Aéreo", Aéreo);
+            diccionarioMovimientos.Add("Colmillo venenoso", Colmilloveneno);
+            diccionarioMovimientos.Add("Puño dinámico", Puñodinámico);
+            diccionarioMovimientos.Add("GarraDragon", GarraDragon);
+            diccionarioMovimientos.Add("Confusion", Confusion);
+            diccionarioMovimientos.Add("LanzaRocas", LanzaRocas);
+            diccionarioMovimientos.Add("Dormir", Dormir);
+            diccionarioMovimientos.Add("Envenenar", Envenenar);
+            diccionarioMovimientos.Add("Quemar", Quemar);
+            diccionarioMovimientos.Add("Paralizar", Paralizar);
+            diccionarioMovimientos.Add("PuñoHielo", PuñoHielo);
+            diccionarioMovimientos.Add("Aguacero", Aguacero);
+            diccionarioMovimientos.Add("PuñoEléctrico", PuñoElectrico);
+            diccionarioMovimientos.Add("PuñoAéreo", PuñoAereo);
+            diccionarioMovimientos.Add("Hoja Afilada", HojaAfilada);
+            diccionarioMovimientos.Add("Cola Larga", ColaLarga);
+            diccionarioMovimientos.Add("Puño Lucha", PuñoLucha);
+            diccionarioMovimientos.Add("Puño Fuego", PuñoFuego);
+            diccionarioMovimientos.Add("Rayo", Rayo);
+
+            // Agregar los pokemons al diccionario
+            diccionarioPokemon.Add("Venusaur", venusaur);
+            diccionarioPokemon.Add("Charizard", charizard);
+            diccionarioPokemon.Add("Blastoise", blastoise);
+            diccionarioPokemon.Add("Pikachu", pikachu);
+            diccionarioPokemon.Add("Pidgeot", pidgeot);
+            diccionarioPokemon.Add("Butterfree", butterfree);
+            diccionarioPokemon.Add("Dragonite", dragonite);
+            diccionarioPokemon.Add("Articuno", articuno);
+            diccionarioPokemon.Add("Machamp", machamp);
+            diccionarioPokemon.Add("Alakazam", alakazam);
+            diccionarioPokemon.Add("Gengar", gengar);
+            diccionarioPokemon.Add("Onix", onix);
+            diccionarioPokemon.Add("Groudon", groudon);
+            diccionarioPokemon.Add("Muk", muk);
+            diccionarioPokemon.Add("Snorlax", snorlax);
+            diccionarioPokemon.Add("Clefable", clefable);
+            diccionarioPokemon.Add("Steelix", steelix);
+            diccionarioPokemon.Add("Kyogre", kyogre);
+            diccionarioPokemon.Add("Roserade", roserade);
+            diccionarioPokemon.Add("Jolteon", jolteon);
+            diccionarioPokemon.Add("Zapdos", zapdos);
+            diccionarioPokemon.Add("Salamence", salamence);
+            diccionarioPokemon.Add("Moltres", moltres);
         }
     }
 }
