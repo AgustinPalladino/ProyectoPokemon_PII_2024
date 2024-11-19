9f95af20130dc9582588d553acc0e0d9492ced5c
Rodrigofagundez2004 <yeekko19@gmail.com>
2024-11-19 03:16:39 -0300

    txt imple

--- Library/DiccionariosYOperacionesStatic.cs
+++ Library/DiccionariosYOperacionesStatic.cs
@@ -1,46 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace Library
 {
     public static class DiccionariosYOperacionesStatic
     {
-        public static int numeroAleatorio(int minimo, int maximo)
-        {
-            Random random = new Random();
-            return random.Next(minimo, maximo + 1);
-        }
+        // Diccionarios para almacenar Pokémon y movimientos
+        public static Dictionary<string, Pokemon> DiccionarioPokemon = new Dictionary<string, Pokemon>();
+        public static Dictionary<string, Movimiento> DiccionarioMovimientos = new Dictionary<string, Movimiento>();
 
         private static Random randomDouble = new Random();
 
-
-        public static double Precision(double precision, int ataqueBase)
-        {
-            double probabilidad = randomDouble.NextDouble(); /// Devuelve valor entre 0 y 1
-            if (probabilidad <= precision)
-            {
-                return ataqueBase;
-            }
-            else
-            {
-                Console.WriteLine("El ataque falló");
-                return 0;
-            }
-        }
-
-
-        public static double CalcularCritico(int precisionMovimiento)
-        {
-            int probabilidad = numeroAleatorio(0, precisionMovimiento);
-            double probabilidadCritico = precisionMovimiento / 10;
-            if (probabilidad <= probabilidadCritico) // Calculo de que la cantidad sea golpe critico
-            {
-                Console.WriteLine("¡Golpe crítico!");
-                return 1.2; // Multiplicador de 20% mas de daño
-            }
-
-            return 1.0; // Sino es critico, pega lo mismo
-        }
-
-
-        /// Diccionario para definir bonificaciones de tipo
+        // Diccionario para definir bonificaciones de tipo
         private static readonly Dictionary<(string tipoMovimiento, string tipoPokemonDefensor), double>
             bonificacionesTipos =
                 new Dictionary<(string, string), double>
@@ -127,7 +99,6 @@ namespace Library
                     { ("Tierra", "Volador"), 0.5 }
                 };
 
-
         /// <summary>
         /// Método para obtener la bonificación de tipo
         /// </summary>
@@ -146,245 +117,86 @@ namespace Library
             return 1; /// Si no hay bonificación específica, el daño es normal
         }
 
-        public static Dictionary<string, Movimiento> DiccionarioMovimientos = new()
+        // Método para cargar Pokémon desde un archivo
+        public static void CargarPokemonDesdeArchivo(string rutaArchivo)
         {
-            { "Arañazo", new Movimiento("Arañazo", 5, 80, "Fuego", false) },
-            { "Lanzallamas", new Movimiento("Lanzallamas", 20, 80, "Fuego", false) },
-            { "Lluevehojas", new Movimiento("Lluevehojas", 20, 100, "Planta", false) },
-            { "Hidrocañon", new Movimiento("Hidrocañon", 20, 60, "Agua", false) },
-            { "PuñoFuego", new Movimiento("PuñoFuego", 20, 60, "Fuego", false) },
-            { "Electrocañon", new Movimiento("Electrocañon", 100, 40, "Eléctrico", false) },
-            { "Aéreo", new Movimiento("Aéreo", 15, 60, "Volador", false) },
-            { "ColmilloVenenoso", new Movimiento("ColmilloVenenoso", 15, 60, "Veneno", false) },
-            { "PuñoDinámico", new Movimiento("PuñoDinámico", 25, 100, "Lucha", false) },
-            { "GarraDragon", new Movimiento("GarraDragon", 30, 60, "Lucha", false) },
-            { "Confusion", new Movimiento("Confusion", 50, 60, "Psíquico", false) },
-            { "lanzarrocas", new Movimiento("lanzarrocas", 20, 80, "Tierra", false) },
-            { "Dormir", new Movimiento("Dormir", 0, 100, "Psíquico", true) },
-            { "Envenenar", new Movimiento("Envenenar", 0, 100, "Veneno", true) },
-            { "Quemar", new Movimiento("Quemar", 0, 100, "Fuego", true) },
-            { "Paralizar", new Movimiento("Paralizar", 0, 100, "Eléctrico", true) },
-            { "PuñoHielo", new Movimiento("Puño Hielo", 20, 80, "Hielo", false) },
-            { "Aguacero", new Movimiento("Aguacero", 20, 60, "Agua", false) },
-            { "PuñoEléctrico", new Movimiento("PuñoEléctrico", 20, 60, "Eléctrico", false) },
-            { "PuñoAéreo", new Movimiento("PuñoAéreo", 15, 80, "Volador", false) },
-            { "HojaAfilada", new Movimiento("HojaAfilada", 25, 60, "Planta", false) },
-            { "ColaLarga", new Movimiento("ColaLarga", 25, 60, "Dragón", false) },
-            { "PuñoLucha", new Movimiento("PuñoLucha", 25, 60, "Lucha", false) },
-            { "Rayo", new Movimiento("Rayo", 20, 100, "Eléctrico", false) }
-        };
-        
-        public static Dictionary<string, Pokemon> DiccionarioPokemon = new()
-        {
+            if (!File.Exists(rutaArchivo))
             {
-                "Venusaur", new Pokemon("Venusaur", "Planta", 120, 40, 60).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Lluevehojas"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Quemar"]
-                    })
-            },
-            {
-                "Charizard", new Pokemon("Charizard", "Fuego", 100, 60, 40).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Lanzallamas"],
-                        DiccionarioMovimientos["Paralizar"],
-                        DiccionarioMovimientos["Quemar"]
-                    })
-            },
-            {
-                "Blastoise", new Pokemon("Blastoise", "Agua", 110, 50, 50).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Hidrocañon"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Envenenar"]
-                    })
-            },
-            {
-                "Pikachu", new Pokemon("Pikachu", "Eléctrico", 80, 55, 40).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Electrocañon"],
-                        DiccionarioMovimientos["Envenenar"],
-                        DiccionarioMovimientos["Paralizar"]
-                    })
-            },
-            {
-                "Pidgeot", new Pokemon("Pidgeot", "Volador", 85, 70, 50).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Aéreo"],
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Paralizar"]
-                    })
-            },
-            {
-                "Butterfree", new Pokemon("Butterfree", "Bicho", 60, 50, 40).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["ColmilloVenenoso"],
-                        DiccionarioMovimientos["Aéreo"],
-                        DiccionarioMovimientos["Quemar"],
-                        DiccionarioMovimientos["Dormir"]
-                    })
-            },
-            {
-                "Dragonite", new Pokemon("Dragonite", "Dragón", 150, 80, 70).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["PuñoFuego"],
-                        DiccionarioMovimientos["GarraDragon"],
-                        DiccionarioMovimientos["Paralizar"],
-                        DiccionarioMovimientos["PuñoAéreo"]
-                    })
-            },
-            {
-                "Articuno", new Pokemon("Articuno", "Hielo", 120, 90, 70).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Aéreo"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Envenenar"]
-                    })
-            },
-            {
-                "Machamp", new Pokemon("Machamp", "Lucha", 130, 100, 60).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["PuñoLucha"],
-                        DiccionarioMovimientos["PuñoDinámico"],
-                        DiccionarioMovimientos["Paralizar"],
-                        DiccionarioMovimientos["Quemar"]
-                    })
-            },
-            {
-                "Alakazam", new Pokemon("Alakazam", "Psíquico", 90, 80, 50).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Confusion"],
-                        DiccionarioMovimientos["Quemar"],
-                        DiccionarioMovimientos["Dormir"]
-                    })
-            },
-            {
-                "Gengar", new Pokemon("Gengar", "Fantasma", 100, 60, 40).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Confusion"],
-                        DiccionarioMovimientos["Quemar"],
-                        DiccionarioMovimientos["Envenenar"]
-                    })
-            },
-            {
-                "Onix", new Pokemon("Onix", "Roca", 110, 50, 50).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["lanzarrocas"],
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Envenenar"]
-                    })
-            },
-            {
-                "Groudon", new Pokemon("Groudon", "Tierra", 150, 80, 70).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["lanzarrocas"],
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Envenenar"],
-                        DiccionarioMovimientos["Paralizar"]
-                    })
-            },
-            {
-                "Muk", new Pokemon("Muk", "Veneno", 85, 70, 50).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["ColmilloVenenoso"],
-                        DiccionarioMovimientos["Confusion"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Quemar"]
-                    })
-            },
-            {
-                "Snorlax", new Pokemon("Snorlax", "Normal", 160, 110, 75).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["PuñoDinámico"],
-                        DiccionarioMovimientos["Envenenar"],
-                        DiccionarioMovimientos["Rayo"]
-                    })
-            },
-            {
-                "Clefable", new Pokemon("Clefable", "Hada", 130, 85, 70).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Envenenar"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Quemar"]
-                    })
-            },
-            {
-                "Steelix", new Pokemon("Steelix", "Acero", 150, 100, 70).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["lanzarrocas"],
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Envenenar"]
-                    })
-            },
-            {
-                "Kyogre", new Pokemon("Kyogre", "Agua", 130, 100, 60).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Hidrocañon"],
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Dormir"],
-                        DiccionarioMovimientos["Aguacero"]
-                    })
-            },
-            {
-                "Roserade", new Pokemon("Roserade", "Planta", 80, 60, 50).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["ColmilloVenenoso"],
-                        DiccionarioMovimientos["Confusion"],
-                        DiccionarioMovimientos["Quemar"],
-                        DiccionarioMovimientos["HojaAfilada"]
-                    })
-            },
-            {
-                "Jolteon", new Pokemon("Jolteon", "Eléctrico", 65, 60, 50).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Electrocañon"],
-                        DiccionarioMovimientos["PuñoEléctrico"],
-                        DiccionarioMovimientos["Paralizar"]
-                    })
-            },
-            {
-                "Zapdos", new Pokemon("Zapdos", "Eléctrico", 120, 80, 70).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Rayo"],
-                        DiccionarioMovimientos["Aéreo"],
-                        DiccionarioMovimientos["PuñoEléctrico"],
-                        DiccionarioMovimientos["Dormir"]
-                    })
-            },
-            {
-                "Salamence", new Pokemon("Salamence", "Dragón", 130, 100, 70).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["PuñoFuego"],
-                        DiccionarioMovimientos["GarraDragon"],
-                        DiccionarioMovimientos["ColaLarga"],
-                        DiccionarioMovimientos["Dormir"]
-                    })
-            },
-            {
-                "Moltres", new Pokemon("Moltres", "Fuego", 125, 85, 75).AgregarMovimientos(new List<Movimiento>
-                    {
-                        DiccionarioMovimientos["Arañazo"],
-                        DiccionarioMovimientos["Aéreo"],
-                        DiccionarioMovimientos["Quemar"],
-                        DiccionarioMovimientos["PuñoFuego"]
-                    })
+                Console.WriteLine($"El archivo {rutaArchivo} no existe.");
+                return;
             }
-        };
+
+            var lineas = File.ReadAllLines(rutaArchivo);
+            foreach (var linea in lineas)
+            {
+                var datos = linea.Split(';');
+                if (datos.Length != 5)
+                {
+                    Console.WriteLine($"Formato incorrecto en línea: {linea}");
+                    continue;
+                }
+
+                var pokemon = new Pokemon(datos[0], datos[1], int.Parse(datos[2]), int.Parse(datos[3]), int.Parse(datos[4]));
+                DiccionarioPokemon[datos[0]] = pokemon; // Usar el nombre como clave
+            }
+        }
+
+        // Método para cargar movimientos desde un archivo
+        public static void CargarMovimientosDesdeArchivo(string rutaArchivo)
+        {
+            if (!File.Exists(rutaArchivo))
+            {
+                Console.WriteLine($"El archivo {rutaArchivo} no existe.");
+                return;
+            }
+
+            var lineas = File.ReadAllLines(rutaArchivo);
+            foreach (var linea in lineas)
+            {
+                var datos = linea.Split(';');
+                if (datos.Length != 5)
+                {
+                    Console.WriteLine($"Formato incorrecto en línea: {linea}");
+                    continue;
+                }
+
+                var movimiento = new Movimiento(datos[0], int.Parse(datos[1]), int.Parse(datos[2]), datos[3], bool.Parse(datos[4]));
+                DiccionarioMovimientos[datos[0]] = movimiento; // Usar el nombre como clave
+            }
+        }
+
+        // Métodos adicionales
+        public static int numeroAleatorio(int minimo, int maximo)
+        {
+            Random random = new Random();
+            return random.Next(minimo, maximo + 1);
+        }
+
+        public static double Precision(double precision, int ataqueBase)
+        {
+            double probabilidad = randomDouble.NextDouble(); // Devuelve valor entre 0 y 1
+            if (probabilidad <= precision)
+            {
+                return ataqueBase;
+            }
+            else
+            {
+                Console.WriteLine("El ataque falló");
+                return 0;
+            }
+        }
+
+        public static double CalcularCritico(int precisionMovimiento)
+        {
+            int probabilidad = numeroAleatorio(0, precisionMovimiento);
+            double probabilidadCritico = precisionMovimiento / 10;
+            if (probabilidad <= probabilidadCritico) // Cálculo de golpe crítico
+            {
+                Console.WriteLine("¡Golpe crítico!");
+                return 1.2; // Multiplicador de 20% más de daño
+            }
+
+            return 1.0; // Sino es crítico, pega lo mismo
+        }
     }
 }
-
-
--- Program/Juego.cs
+++ Program/Juego.cs
@@ -1,5 +1,23 @@
 ﻿using Library;
-Combate combate = new Combate();
-Jugador j1 = new Jugador("Jugador 1");
-Jugador j2 = new Jugador("Jugador 2");
-combate.BuclePrincipal(j1,j2);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ruta correcta a los archivos
+        string rutaPokemon = @"C:\Users\rf4tc\Desktop\Proyecto p2\ProyectoPokemon_PII_2024\pokemones.txt";
+        string rutaMovimientos = @"C:\Users\rf4tc\Desktop\Proyecto p2\ProyectoPokemon_PII_2024\movimientos.txt";
+
+        // Cargar los archivos de Pokémon y movimientos
+        DiccionariosYOperacionesStatic.CargarPokemonDesdeArchivo(rutaPokemon);
+        DiccionariosYOperacionesStatic.CargarMovimientosDesdeArchivo(rutaMovimientos);
+
+        // Crear jugadores y combate
+        Combate combate = new Combate();
+        Jugador j1 = new Jugador("Jugador 1");
+        Jugador j2 = new Jugador("Jugador 2");
+
+        // Iniciar el combate
+        combate.BuclePrincipal(j1, j2);
+    }
+}
