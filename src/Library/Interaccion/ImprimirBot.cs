2468c6bff2331c4ab10eff93fb77edb9ec13f317
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-11-25 16:57:49 -0300

    .

---
+++ src/Library/Interaccion/ImprimirBot.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Ucu.Poo.DiscordBot.Domain;
+
+namespace Ucu.Poo.DiscordBot.Commands;
+
+/// <summary>
+/// Esta clase implementa el comando 'battle' del bot. Este comando une al
+/// jugador que envía el mensaje con el oponente que se recibe como parámetro,
+/// si lo hubiera, en una batalla; si no se recibe un oponente, lo une con
+/// cualquiera que esté esperando para jugar.
+/// </summary>
+// ReSharper disable once UnusedType.Global
+public class ImprimirBot : ModuleBase<SocketCommandContext>
+{
+    /// <summary>
+    /// Implementa el comando 'battle'. Este comando une al jugador que envía el
+    /// mensaje a la lista de jugadores esperando para jugar.
+    /// </summary>
+    // ReSharper disable once UnusedMember.Global
+    public async Task ExecuteAsync(string mensaje,
+        [Remainder]
+        [Summary("Display name del oponente, opcional")]
+        string? opponentDisplayName = null)
+    {
+        string displayName = CommandHelper.GetDisplayName(Context);
+        
+        SocketGuildUser? opponentUser = CommandHelper.GetUser(
+            Context, opponentDisplayName);
+
+
+        if (opponentUser != null)
+        {
+            await Context.Message.Author.SendMessageAsync(mensaje);
+        }
+        else
+        {
+            mensaje = $"No hay un usuario {opponentDisplayName}";
+        }
+
+        await ReplyAsync(mensaje);
+    }
+}
--- src/Library/Interaccion/InteraccionPorBot.cs
+++ src/Library/Interaccion/InteraccionPorBot.cs
@@ -1,48 +1,19 @@
 ﻿using Discord.WebSocket;
+using Ucu.Poo.DiscordBot.Commands;
 
 namespace Ucu.Poo.DiscordBot.Interaccion;
 
 public class InteraccionPorBot : IInteraccionConUsuario
 {
-    private readonly DiscordSocketClient client;
-    private readonly SocketTextChannel channel;
-    private IInteraccionConUsuario _interaccionConUsuarioImplementation;
-
-    public InteraccionPorBot(DiscordSocketClient client, SocketTextChannel channel)
-    {
-        this.client = client;
-        this.channel = channel;
-    }
 
     public void ImprimirMensaje(string mensaje)
     {
-        this.channel.SendMessageAsync(mensaje).Wait();
+        ImprimirBot imprimirBot = new ImprimirBot();
+        imprimirBot.ExecuteAsync(mensaje);
     }
 
     public string LeerEntrada()
     {
-        return EsperarRespuestaAsync().Result;
-    }
-
-    private async Task<string> EsperarRespuestaAsync()
-    {
-        var tcs = new TaskCompletionSource<string>();
-        
-        Task MessageHandler(SocketMessage message)
-        {
-            if (message.Channel.Id == this.channel.Id && !message.Author.IsBot)
-            {
-                tcs.SetResult(message.Content);
-            }
-            return Task.CompletedTask;
-        }
-        
-        this.client.MessageReceived += MessageHandler;
-        
-        var result = await tcs.Task;
-        
-        this.client.MessageReceived -= MessageHandler;
-
-        return result;
+        return "";
     }
 }
