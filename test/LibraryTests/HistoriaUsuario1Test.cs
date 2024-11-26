e516d1cc05c5f527ae285eef3ea017644a17e391
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-11-26 16:29:17 -0300

    Test de seleccionar pokemon de cambio

--- test/LibraryTests/HistoriaUsuario1Test.cs
+++ test/LibraryTests/HistoriaUsuario1Test.cs
@@ -9,7 +9,7 @@ public class HisotriaUsuario1Test
     private Jugador jugador2;
     private IInteraccionConUsuario mockInteraccion;
     private Logica logica;
-    
+
     [Test]
     public void SeleccionarPokemonDeCambio_CambiaPokemonCorrectamente()
     {
@@ -18,19 +18,44 @@ public class HisotriaUsuario1Test
         logica = new Logica(mockInteraccion); // Pasa el mock al constructor
 
         Jugador jugador = new Jugador("Jugador");
-        Pokemon charizard = new Pokemon("Charizard", "Fuego", 120, 80, 100);
-        jugador.agregarPokemon(charizard);
+        Pokemon pokemon = new Pokemon("Charizard", "Fuego", 120, 80, 100);
+        Pokemon pokemon2 = new Pokemon("Venusaur", "Planta", 100, 80, 200);
+        jugador.agregarPokemon(pokemon);
 
         // Simula la entrada del usuario
-        mockInteraccion.LeerEntrada().Returns("Charizard");
+        mockInteraccion.LeerEntrada().Returns("Charizard", "0", "Error", "Venusaur");
 
-        // Act
+
+        // EL pokemon cambio correctamente
         bool resultado = logica.SeleccionarPokemonDeCambio(jugador);
-
-        // Assert
         Assert.That(resultado, Is.True, "El Pokémon debería haber sido cambiado correctamente.");
         mockInteraccion.Received(1).LeerEntrada(); // Verifica que se llamó al método LeerEntrada
-        Assert.That(jugador.pokemonEnCancha().Nombre, Is.EqualTo("Charizard"), "El Pokémon actual no fue cambiado correctamente.");
+        Assert.That(resultado, Is.EqualTo(true),
+            "El Pokémon actual fue cambiado correctamente.");
+
+        
+        
+        // Volvio hacia atras
+        resultado = logica.SeleccionarPokemonDeCambio(jugador);
+        Assert.That(resultado, Is.False, "Usted deberia haber vuelto hacia atras");
+        mockInteraccion.Received(2).LeerEntrada(); // Verifica que se llamó al método LeerEntrada
+        Assert.That(resultado, Is.EqualTo(false), "Usted regreso hacia atras");
+        
+        // Error mal nombre
+        resultado = logica.SeleccionarPokemonDeCambio(jugador);
+        Assert.That(resultado, Is.False, "Sale un error");
+        mockInteraccion.Received(3).LeerEntrada(); // Verifica que se llamó al método LeerEntrada
+        Assert.That(resultado, Is.EqualTo(false), "Error inesperado");
+
+        
+        // El pokemon cambio correctamente
+        jugador.equipoPokemon[0] = null;
+        jugador.equipoPokemon.Add(pokemon);
+        jugador.equipoPokemon.Add(pokemon2);
+        resultado = logica.SeleccionarPokemonDeCambio(jugador);
+        Assert.That(resultado, Is.True, "El Pokémon debería haber sido cambiado correctamente.");
+        mockInteraccion.Received(4).LeerEntrada(); // Verifica que se llamó al método LeerEntrada
+        Assert.That(resultado, Is.EqualTo(true),
+            "El Pokémon actual fue cambiado correctamente.");
     }
-    
 }
