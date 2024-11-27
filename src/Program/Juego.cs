f0cc910d1b179caeddec0ee383ec9a5f5a9742a3
Rodrigofagundez2004 <yeekko19@gmail.com>
2024-11-27 20:24:46 -0300

    defensa

--- src/Program/Juego.cs
+++ src/Program/Juego.cs
@@ -16,29 +16,28 @@ internal static class Juego
     private static void Main()
     {
         //Juego por consola:
-        /*
         Jugador j1 = new Jugador("Jugador 1");
         Jugador j2 = new Jugador("Jugador 2");
         Combate combate = new Combate(new InteraccionPorConsola());
         combate.BuclePrincipal(j1, j2);
-        */
+        
         
         
         // DemoFacade();
         // DemoBot();
     }
 
-    private static void DemoFacade()
+    //private static void DemoFacade()
     {
-        Console.WriteLine(Facade.Instance.AddTrainerToWaitingList("player"));
-        Console.WriteLine(Facade.Instance.AddTrainerToWaitingList("opponent"));
-        Console.WriteLine(Facade.Instance.GetAllTrainersWaiting());
-        Console.WriteLine(Facade.Instance.StartBattle("player", "opponent"));
-        Console.WriteLine(Facade.Instance.GetAllTrainersWaiting());
+       // Console.WriteLine(Facade.Instance.AddTrainerToWaitingList("player"));
+        //Console.WriteLine(Facade.Instance.AddTrainerToWaitingList("opponent"));
+       // Console.WriteLine(Facade.Instance.GetAllTrainersWaiting());
+       // Console.WriteLine(Facade.Instance.StartBattle("player", "opponent"));
+       // Console.WriteLine(Facade.Instance.GetAllTrainersWaiting());
     }
 
-    private static void DemoBot()
+    //private static void DemoBot()
     {
-        BotLoader.LoadAsync().GetAwaiter().GetResult();
+        //BotLoader.LoadAsync().GetAwaiter().GetResult();
     }
 }
